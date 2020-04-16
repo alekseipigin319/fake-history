a45193bf0226c322125c556cf98013f7132c2b4a
alekseipigin319 <alekseipigin319@gmail.com>
2020-04-16 15:55:00 +0000

    I'll bypass the multi-byte OCR sensor, that should transmitter the RSS monitor!

--- generate-fake-history.js
+++ generate-fake-history.js
@@ -1,8 +1,8 @@
 const { execSync } = require("child_process");
 const { faker } = require("@faker-js/faker");
 
-const startDate = new Date("2017-04-18");
-const endDate = new Date("2020-04-17");
+const startDate = new Date("2020-04-17");
+const endDate = new Date("2023-04-17");
 let currentDate = new Date(startDate);
 
 while (currentDate <= endDate) {
